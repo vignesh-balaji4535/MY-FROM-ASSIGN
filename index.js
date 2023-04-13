b7276a2537bcbd8f13ea8fecb31d33203f2a44ce
vignesh-balaji4535 <127295249+vignesh-balaji4535@users.noreply.github.com>
2023-04-13 09:24:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function foo() {
+    let firstName=document.getElementById("fname").value;
+    console.log(`firstNamem :${firstName}`)
+    let secorndName=document.getElementById("sname").value;
+    console.log(`secorndName :${secorndName}`)
+    let emailid=document.getElementById("mail").value;
+    console.log(`emailID :${emailid}`)
+    let password=document.getElementById("pass").value;
+    console.log(`password :${password}`)
+    let mobileNo=document.getElementById("num").value;
+    console.log(`mobileNo :${mobileNo}}`)
+    let address=document.getElementById("add").value;
+    console.log(`address :${address}`)
+    
+}
